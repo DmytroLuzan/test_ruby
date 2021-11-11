90dda2c7dcdc923fa5ef2e69df4f2df30350f34b
dmytroluzan <dimaluzan9@gmail.com>
2021-11-11 13:48:11 +0200

    edit file graphql.js

--- app/javascript/packs/graphql.js
+++ app/javascript/packs/graphql.js
@@ -1,7 +1,7 @@
 import graphql from 'graphql.js'
 import { getToken } from './utils.js'
 
-export const gql = graphql('http://localhost:3000/graphql', {
+export const gql = graphql(`${location.origin}/graphql`, {
   method: 'POST', // POST by default.
   headers: {
     'X-CSRF-Token': getToken(), // need for BE side
