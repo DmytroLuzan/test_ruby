4bbfa6656d8b5e56d982a9ad538f5428418a3d4d
dmytroluzan <dimaluzan9@gmail.com>
2021-11-17 00:38:22 +0200

    add VCR.use_cassette

--- spec/graphql/queries/user/user_spec.rb
+++ spec/graphql/queries/user/user_spec.rb
@@ -4,17 +4,27 @@ module Query
   module User
     RSpec.describe User, type: :request do
       describe '.resolve' do
-        it 'get user name and repos' do
-          login = "user"
+        let(:data) do
+          VCR.use_cassette('users', record: :new_episodes) do
+            login = 'dhh'
+            post '/graphql', params: { query: query(login: login) }
+            json = JSON.parse(response.body)
+            json['data']['user']
+          end
+        end
 
-          post '/graphql', params: { query: query(login: login) }
-          json = JSON.parse(response.body)
-          data = json['data']['user']
-          puts data
-          expect(data).to include(
-            'name' => be_present,
-            'repos' => be_present # add array check
-          )
+        it 'get user object' do
+          expect(data).to be_kind_of(Hash)
+        end
+
+        it 'user object has name' do
+          expect(data).to have_key('name')
+          expect(data['name']).to be_kind_of(String)
+        end
+
+        it 'user object has repos' do
+          expect(data).to have_key('repos')
+          expect(data['repos']).to be_kind_of(Array)
         end
       end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'vcr'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -30,6 +31,13 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/vcr"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
